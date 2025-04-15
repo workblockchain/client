ae1c8afca803388a60451ffab6824466875af4aa
Ri0n72Y <tzy673570751@gmail.com>
2025-04-15 18:46:46 +0800

    feat: button and timer refactor: minor

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,59 @@
+import type {Meta, StoryObj} from "@storybook/react"
+import {Button} from "./Button"
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    $variant: {
+      control: {type: "radio"},
+      options: ["solid", "outline", "text"],
+    },
+    $size: {
+      control: {type: "radio"},
+      options: ["small", "medium", "large"],
+    },
+    colorScheme: {
+      control: {type: "radio"},
+      options: ["primary"],
+    },
+    disabled: {
+      control: "boolean",
+    },
+    children: {
+      control: "text",
+    },
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: "Primary Button",
+    $variant: "solid",
+  },
+}
+
+export const Outline: Story = {
+  args: {
+    ...Primary.args,
+    $variant: "outline",
+  },
+}
+
+export const Text: Story = {
+  args: {
+    ...Primary.args,
+    $variant: "text",
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    ...Primary.args,
+    disabled: true,
+  },
+}
