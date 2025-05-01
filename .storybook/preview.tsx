2e3048aa079faf42bda5ade86017cdff8b263cd7
James Aprosail <aprosail@outlook.com>
2025-05-01 21:13:28 +0800

    fix: apply root theme into storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,7 @@ import type {Preview} from "@storybook/react"
 import React from "react"
 import {ThemeProvider} from "styled-components"
 import "../src/i18n"
+import "../src/root.css"
 import {GlobalStyles} from "../src/styles/globalStyles"
 import {theme} from "../src/styles/theme"
 
