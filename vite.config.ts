af59102bec7ec84a85f76f03503a0ebddff8abdf
James Aprosail <aprosail@outlook.com>
2025-05-01 18:46:06 +0800

    perf: optimize vite build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from "@vitejs/plugin-react-swc"
+import {join} from "node:path"
 import {fileURLToPath, URL} from "node:url"
 import {defineConfig} from "vite"
 import svgr from "vite-plugin-svgr"
+
+const root = import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +13,17 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: join(root, "dist"),
+    rollupOptions: {
+      output: {
+        assetFileNames: "[hash].[ext]",
+        chunkFileNames: "[hash].js",
+        entryFileNames: "[hash].js",
+      },
     },
   },
 })
