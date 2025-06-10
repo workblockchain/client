28dcdb7b27e21cd720bdbe5033475fc4e7dedd19
Ri0n72Y <tzy673570751@gmail.com>
2025-06-10 18:27:51 +0800

    fix: style

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
