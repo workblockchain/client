daf606af1fd00aae5562bcd813d76388ccdac22e
SmallAirplaneX <2321764325@qq.com>
2025-06-27 16:21:38 +0800

    fix: 使用自动生成的方式

--- src/components/Icons/svgIcons.tsx
+++ src/components/Icons/svgIcons.tsx
@@ -14,8 +14,8 @@
 // See the Mulan PubL v2 for more details.
 //
 // === Auto generated, DO NOT EDIT ABOVE ===
-import ArrowRound from "@/assets/arrow-round.svg?react"
 import Arrow from "@/assets/arrow.svg?react"
+import ArrowRound from "@/assets/arrow-round.svg?react"
 import Check from "@/assets/check.svg?react"
 import Clock from "@/assets/clock.svg?react"
 import Cross from "@/assets/cross.svg?react"
@@ -26,29 +26,32 @@ import List from "@/assets/list.svg?react"
 import Logo from "@/assets/logo.svg?react"
 import Navigation from "@/assets/navigation.svg?react"
 import People from "@/assets/people.svg?react"
-import PlusIcon from "@/assets/plus.svg?react"
+import Plus from "@/assets/plus.svg?react"
 import Pomodoro from "@/assets/pomodoro.svg?react"
 import ReactIcon from "@/assets/react.svg?react"
-import RightIcon from "@/assets/right.svg?react"
+import Right from "@/assets/right.svg?react"
 import Unchecked from "@/assets/unchecked.svg?react"
+import Vector from "@/assets/vector.svg?react"
 import Workbench from "@/assets/workbench.svg?react"
+
 export const svgIcons = {
+  Workbench,
+  Vector,
+  Unchecked,
+  Right,
   ReactIcon,
   Pomodoro,
+  Plus,
   People,
   Navigation,
+  Logo,
   List,
   Key,
+  Kanban,
   Gear,
   Cross,
   Clock,
   Check,
   Arrow,
   ArrowRound,
-  PlusIcon,
-  RightIcon,
-  Workbench,
-  Logo,
-  Unchecked,
-  Kanban,
 }
