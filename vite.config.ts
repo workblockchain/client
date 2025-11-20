160137a160d41d48e53493abe4ec5b45eacfab25
Bed_King <2321764325@qq.com>
2025-11-20 19:22:49 +0800

    chore:  package data

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react"
 import {fileURLToPath, URL} from "node:url"
 import {defineConfig} from "vite"
 import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      plugins: [["@swc/plugin-styled-components", {}]],
-    }),
-    svgr(),
-  ],
+  plugins: [react(), svgr()],
   base: "./",
   resolve: {
     alias: {
